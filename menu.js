f8482c5aa571a29f35f9c6c4f1964f55ecfefa3d
Fatima Faisal <fatima_el_105@yahoo.com>
2023-06-22 12:09:53 +0100

    Updated navbar

--- menu.js
+++ menu.js
@@ -1,11 +1,13 @@
 const hamMenu = document.querySelector('.hamburger-menu');
 const menu = document.querySelector('.menu');
+const nav = document.querySelector('.navbar');
 const logo = document.getElementById("logo");
 
 
 hamMenu.addEventListener('click', ()=>{
   hamMenu.classList.toggle('active');
   menu.classList.toggle('active');
+  nav.classList.toggle('active');
 })
 
 logo.addEventListener("click", function(){
